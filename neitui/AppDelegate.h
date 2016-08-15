71109dead5189cb43716eade67e46efbae48b4c4
hzf <1810022686@qq.com>
2016-08-15 11:01:03 +0800

    Initial Commit

---
+++ neitui/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  neitui
+//
+//  Created by hzf on 16/8/15.
+//  Copyright © 2016年 neitui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ neitui/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  neitui
+//
+//  Created by hzf on 16/8/15.
+//  Copyright © 2016年 neitui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
